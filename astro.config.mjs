444e561276f64c228eab13f0ed152e19e47d0ed7
M. David Ramiro <dramirobarragan@gmail.com>
2023-10-15 10:33:45 +0200

    fix names

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,7 @@ const SERVER_PORT = 3000;
 // the url to access your blog during local development
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 // the url to access your blog after deploying it somewhere (Eg. Netlify)
-const LIVE_URL = "https://dramiro.com";
+const LIVE_URL = "https://davidramiro.com";
 // this is the astro command your npm script runs
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
@@ -47,7 +47,7 @@ export default defineConfig({
         prefer_related_applications: false,
         name: "davidramiro",
         short_name: "davidramiro",
-        "icons": [
+        icons: [
             {
                 "src": "/android-chrome-192x192.png",
                 "sizes": "192x192",
--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "dramiro.com";
+export const SITE_TITLE = "davidramiro.com";
 export const SITE_DESCRIPTION = "Bienvenido a mi block de notas personal";
 export const TWITTER_HANDLE = "@yourtwitterhandle";
 export const MY_NAME = "David Ramiro";
